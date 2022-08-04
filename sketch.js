9d904828c1de4363397c77b3e398b6cba7091f5a
andressalessa <lessadasilveiraandressa@gmail.com>
2022-08-04 19:24:54 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -22,6 +22,7 @@ function setup()
 
   ground = new Ground(200, 690, 600, 20);
   rope = new Rope(6,{x: 245, y:30});
+  console.log(rope);
   fruit = Bodies.circle(300, 300, 15, {density: 0.001});
   Composite.add(rope.body, fruit);
 
@@ -34,6 +35,7 @@ function draw()
 {
   background(51);
   Engine.update(engine);
+
   rope.show();
   ellipse(fruit.position.x, fruit.position.y, 15, 15);
   ground.show();
